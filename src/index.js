e3e53c843c5f67b0de441f940199cbd74da73e30
Kwamboka <kwambokaj2.jk@gmail.com>
2022-04-27 17:38:10 +0300

    Add remove task functionality

--- src/index.js
+++ src/index.js
@@ -1,38 +1,108 @@
 // css files here
-import './style.css';
+import "./style.css";
 
-const tasks = [
-  { description: 'Mop the house', completed: true, index: 1 },
-  { description: 'Clean utensils', completed: false, index: 2 },
-  { description: 'Read a book', completed: false, index: 3 },
-  { description: 'Prepare Breakfast', completed: true, index: 4 },
-];
+const listContainer = document.getElementById("list-container");
 
-const listContainer = document.getElementById('list-container');
+class ToDoList {
+  constructor() {
+    this.listArray = JSON.parse(localStorage.getItem("toDoList")) || [];
+  }
 
-function showtasks() {
-  let chores = '';
-  tasks.forEach((element) => {
-    chores += `
-            <div class="list">
-                <input type="checkbox">
-                <p>${element.description}</p>
-                <a><i class="fa fa-ellipsis-v fa-2x" aria-hidden="true"></i>
-                </a>
-            </div>`;
-  });
-  listContainer.innerHTML = `
-<div class="list-head">
-<p class="todo">Today's ToDo List</p>
-<i id="icon" class="fa-solid fa-rotate fa-lg font-awesome-icon"></i>
-</div>
-<input type="text" class="input" placeholder="Add to your list...">
-<div class="list-body">
-${chores}</div>
-<div class="button">
+  // showTaskItem(element) {
+  //   return `<div class="list">
+  //               <input type="checkbox" name="check">
+  //               <p class="taskdescription">${element.description}</p>
+  //               <a><i class="fa fa-ellipsis-v fa-2x" aria-hidden="true" aria-label="${element.index}" id="menu-icon"></i>
+  //               </a>
+  //           </div>`;
+  // }
+  // editTaskItem(element) {
+  //   return `<div class="list">
+  //               <input type="checkbox" name="check">
+  //               <input type="text" value="${element.description}">
+  //               <a id="menu-icon" ><i class="fa fa-trash-O fa-2x" aria-hidden="true" onclick="${this.editTask(
+  //                 element.index
+  //               )}"></i>
+  //               </a>
+  //           </div>`;
+  // }
+
+  //show tasks
+  showTasks() {
+    let chores = "";
+    this.listArray.forEach((element) => {
+      chores += `${
+        element.edit ? this.editTaskItem(element) : this.showTaskItem(element)
+      }`;
+    });
+    listContainer.innerHTML = `
+  <div class="list-head">
+  <p class="todo">Today's ToDo List</p>
+  <i id="icon" class="fa-solid fa-rotate fa-lg font-awesome-icon"></i>
+  </div>
+  <input type="text" class="input" name="addtext" placeholder="Add to your list...">
+  <div class="list-body">${chores}</div>
+  <div class="button">
     <button >Clear all completed</button>
-</div>`;
-}
-showtasks();
+  </div>`;
+  }
+
+  //add a task
+  addTask(description) {
+    const task = {
+      description,
+      completed: false,
+      index: this.listArray.length + 1,
+      edit: false,
+    };
+    this.listArray = [...this.listArray, task];
+    localStorage.setItem("toDoList", JSON.stringify(this.listArray));
+    this.showTasks();
+  }
+
+//   //remove a task
+//   removeTask(index) {
+//     this.listArray = this.listArray.filter((l) => l.id !== index);
+//     this.listArray = this.listArray.map((list, i) => {
+//       list.index = i + 1;
+//       return list;
+//     });
+//     localStorage.setItem("toDoList", JSON.stringify(this.listArray));
+//     this.showTasks();
+//   }
+
+//   //Edit a task
+//   editTask(index, description) {
+//     this.listArray[index - 1].description = description;
+//     localStorage.setItem("toDoList", JSON.stringify(this.listArray));
+//     this.showTasks();
+//   }
+//   //
+// }
+
+// const newTask = new ToDoList();
+// newTask.showTasks();
+// const checkbox = document.querySelector("input[name=check]");
+// const taskDescription = document.querySelector(".taskdescription");
+// const addList = document.querySelector("input[name=addtext]");
+// const menuIcon = document.getElementById("menu-icon");
+
+// // checkbox.addEventListener("change", () => {
+// //   taskDescription.style.textDecoration = "line-through";
+// // });
+
+// addList.addEventListener("keydown", (e) => {
+//   if (e.code === "Enter") {
+//     if (addList.value) {
+//       newTask.addTask(addList.value);
+//       newTask.showTasks;
+//       addList.value = "";
+//     }
+//   }
+// });
+
+// menuIcon.addEventListener("click", (e) => {
+//   console.log(e.target.ariaLabel);
+// });
 
 window.onload();
