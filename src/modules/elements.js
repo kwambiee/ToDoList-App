72ab99529d6ba0194abc605529da705e65a3edb1
Kwamboka <kwambokaj2.jk@gmail.com>
2022-04-28 11:15:10 +0300

    Separate elements

---
+++ src/modules/elements.js
@@ -0,0 +1,6 @@
+const listBody = document.getElementById("list-body");
+const checkbox = document.querySelector("input[name=check]");
+const taskDescription = document.querySelector(".taskdescription");
+const addList = document.querySelector("input[name=addtext]");
+
+export { listBody, checkbox, taskDescription, addList };
