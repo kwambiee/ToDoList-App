f86ed69da4159c57a85eb742b10983b366170f16
Kwamboka <kwambokaj2.jk@gmail.com>
2022-04-29 07:32:43 +0300

    Add functionality to clear button, checkbox and complete status

--- src/modules/todolist.js
+++ src/modules/todolist.js
@@ -11,35 +11,9 @@ class ToDoList {
     this.listArray = JSON.parse(localStorage.getItem("toDoList")) || [];
   }
 
-  showTaskItem(element) {
-    return `<div class="list show">
-                <input type="checkbox" name="check">
-                <p class="taskdescription">${element.description}</p>
-                <a><i class="fa fa-ellipsis-v fa-2x menu-icon" aria-label ="${element.index}" ></i>
-                </a>
-            </div>`;
-  }
-
-  editDescription(element) {
-    return `<div class="list edit">
-                <input type="checkbox" name="check">
-                <input type="text" value="${element.description}">
-                <a id="menu-icon" ><i class="fa fa-trash-o fa-2x" aria-hidden="true"></i>
-                </a>
-            </div>`;
-  }
-
   //show tasks
   showTasks() {
-    let chores = "";
-    this.listArray.forEach((element) => {
-      chores += `${
-        element.edit
-          ? this.editDescription(element)
-          : this.showTaskItem(element)
-      }`;
-    });
-    return chores;
+    return JSON.parse(localStorage.getItem("toDoList")) || [];
   }
 
   setEdit(i) {
@@ -63,8 +37,19 @@ class ToDoList {
   }
 
   //remove a task
+  clearCompleted() {
+    this.listArray = this.listArray.filter((item) => item.completed !== true);
+    if (this.listArray.length > 0) {
+      this.listArray = this.listArray.map((list, i) => {
+        list.index = i + 1;
+        return list;
+      });
+    }
+    this.updateTasks();
+  }
+  //remove a task
   removeTask(index) {
-    this.listArray = this.listArray.filter((l) => l.id !== index);
+    this.listArray = this.listArray.filter((item) => item.index !== index);
     this.listArray = this.listArray.map((list, i) => {
       list.index = i + 1;
       return list;
@@ -75,6 +60,16 @@ class ToDoList {
   //Edit a task
   editTask(index, description) {
     this.listArray[index - 1].description = description;
+    this.listArray[index - 1].edit = false;
+    this.updateTasks();
+  }
+
+  changeComplete(i) {
+    let status = this.listArray[i - 1].completed;
+    this.listArray[i - 1] = {
+      ...this.listArray[i - 1],
+      completed: !status,
+    };
     this.updateTasks();
   }
 }
